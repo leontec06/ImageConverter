e2ea2ddd0314dd6e6206d720f6bb61e34c023101
leontec06 <leontec06@gmail.com>
2023-10-22 20:56:27 +0200

    inkscape & readme

--- README.txt
+++ README.txt
@@ -1,1 +1,20 @@
+Dies ist eine kurze Bedienungsanleitung für das Bildumwandlungsprogramm meiner Maturaarbeit "Von der Bilddatei zur Roboterzeichnung.
+
+Für die Umwandlung muss das converter.py Programm gestartet werden. Dabei wird das ebenfalls selbstgeschriebene Halftone Programm 
+importiert und im converter.py verwendet.
+
+Für den Input in der Linie 7 des converter.py sind Bilder im "images" Ordner vorhanden. Je nach Bild mmüssen die Parameter in Linie 8 und 
+12 der converter.py Datei verändert werden. Für einige Bilder sind bereits empfohlene Werte als Kommentare im Programm vermerkt. 
+
+Die aufpoppenden Fenster der Zwischenstufen (Grayscale und Halftone) während dem converter.py Porgramm müssen wieder geschlossen werden, damit das Programm weiterläuft.
+Am Schluss wird in der svg_output.svg Datei ein Modell der Zeichnung gespeichert. Diese Datei kann in einem beliebigen Browser geöffnet werden. 
+Der G-Code für den Plotter wird im Ordner "gcode" in der Datei Gcode.txt abgespeichert. 
+Im Ordner "gcode" befinden sich ebenfalls gcode Vorlagen für den Trooper, David und ein Quadrat, um den Plotter erstmals auszutesten. 
+
+Für die weiteren Zeichnungsmethoden mit Inkscape aus den Kapiteln 3.6.2 und 3.6.3 befinden sich zudem Bilder im Ordner "inkscape_input". Aus diesen kann wiederrum in 
+Inkscape je nach verwendetem Stift G-Code generiert werden. Beispieldateien dazu sind im Ordner "inkscape_output" abgelegt.
+
+Für allfällige Fragen bin ich unter der E-Mail Adresse leontec06@gmail.com kontaktierbar.
+Léon Nedic, 22.10.2023
+
 
--- converter.py
+++ converter.py
@@ -4,8 +4,8 @@ import svgwrite
 
 
 
-img = cv2.imread("images\Trooper.jpg", 0)  # load image "Test.png" in grayscale (0)
-in_img = cv2.resize(img, (401, 401))  # adjust the size of the image, (401, 401) für Trooper.jpg, (361, 461) für david2.png, (401, 531) für Luke_Skywalker.png
+img = cv2.imread("images\Trooper.jpg", 0)  # Laden des Bildes in Grayscale -> ("name.jpg", 0)
+in_img = cv2.resize(img, (401, 401))  # Bildgrösse anpassen, (401, 401) für Trooper.jpg, (361, 461) für david2.png, (401, 531) für Luke_Skywalker.png
 cv2.imshow("grayscale",in_img)
 cv2.waitKey(0)
 rows, cols = in_img.shape
@@ -22,9 +22,9 @@ PenPath = []
 
 cursor = (0, 0)
 
-for y in range(0, rows, 5):  # rows 15
+for y in range(0, rows, 5):  # Reihen in Fünferschritten nach unten
+    
     # Überprüfen, ob in geraden oder ungeraden Reihe (Y-Wert)
-
     if y % 2 == 0:
         # Wenn gerade, starte von der ersten Spalte
         start_col = 0
@@ -37,15 +37,15 @@ for y in range(0, rows, 5):  # rows 15
         step_col = -5 #Parameter für Abstand zwischen den verarbeiteten Bildpunkten
         columns = 0
 
-    for x in range(start_col, columns, step_col):  # cols
+    for x in range(start_col, columns, step_col):  # Iteration durch Kolonnen
 
         if in_img[y, x] == 255:
-            in_img[y, x] = 0
+            in_img[y, x] = 0 #schwarze Punkte werden im Raster gesetzt. Bewirkt nichts,jedoch gut zur Vorstellung wenn noch keine Linien gezeichnet werden
 
 
         PenPath.append([x,y])
 
-        # Check neighbor pixels
+        # Überprüfe Nachbarspixel
         neighbors=[[y - 1, x - 1], [y - 2, x - 2], [y - 1, x],
                    [y - 2, x], [y - 1, x + 1], [y - 2, x + 2],
                    [y, x + 1], [y, x + 2], [y + 1, x + 1],
@@ -54,8 +54,8 @@ for y in range(0, rows, 5):  # rows 15
                    [y, x - 2]]
         if x >= 5 and y >= 5 and x <= cols - 5 and y <= rows - 5:
             for i in neighbors:
-                if in_img[i[0], i[1]] ==0:
-                    PenPath.append([i[1], i[0]])
+                if in_img[i[0], i[1]] ==0: #Überprüfen ob Nachbarspixel schwarz ist
+                    PenPath.append([i[1], i[0]]) #wenn schwarz wird er zum Pfad hinzugefügt
                     cursor = (i[1], i[0])
 
         cursor = (x, y)
@@ -63,21 +63,19 @@ for y in range(0, rows, 5):  # rows 15
 
 
 first=True
-svgdraw = svgwrite.Drawing('svg_output.svg', profile='tiny', size=('120mm', '120mm')) #Datei vorbereiten
+svgdraw = svgwrite.Drawing('svg_output.svg', profile='tiny') #SVG-Datei vorbereiten
 
 old=[0,0]
 for i in PenPath:
-    svgdraw.add(svgdraw.line(start=(old[0], old[1]), end=(i[0], i[1]), stroke=svgwrite.rgb(0, 0, 0), stroke_width=1))
+    svgdraw.add(svgdraw.line(start=(old[0], old[1]), end=(i[0], i[1]), stroke=svgwrite.rgb(0, 0, 0), stroke_width=1)) #Farbe und Dicke der Linie können mittels den zwei letzten Parammetern angepasst werden.
     old=[i[0], i[1]]
     gcode = open("gcode\Gcode.txt", "a")
     if first==True:
-        gcode.write(f"F7000\n")
+        open("gcode\Gcode.txt", 'w').close()
+        gcode = open("gcode\Gcode.txt", "a")
+        gcode.write(f"F7000\n") #Feedrate (Geschwindigkeit) des Plotters wird in der ersten Zeile gesetzt, in diesem Fall 7000 für Genauigkeit und gutes Tempo.
     gcode.write(f"G1 x{i[0]/4} y{i[1]/4}\n")
     gcode.close()
     first=False
 svgdraw.save()
-
-
-
-
 
